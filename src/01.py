d051dadb31c7891da5e38d334d9f4b485977bada
Alexander Raab <alexander.raab1@gmx.de>
2021-12-20 23:27:53 +0100

    Solve Day 1

---
+++ src/01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+ 
+import math
+
+with open("..\input\day_1.txt",'r') as textfile:
+    str_content = textfile.readlines()
+int_content = [int(i) for i in str_content]
+
+#Testing
+#int_content = [199,200,208,210,200,207,240,269,260,263]
+
+## Part 2
+tms = [] #ThreeMeasurementSum
+for i in range(len(int_content)-2):
+    tms.append(sum(int_content[i:i+3]))
+
+
+## Part 1
+pre = math.inf
+counter = 0
+
+for x in tms: #use 'int_content' for part 1 and 'tms' for part 2
+    if x > pre:
+        counter += 1
+    pre = x
+
+print("Occurances: " + str(counter))
