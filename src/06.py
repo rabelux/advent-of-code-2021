9ed6a670ee7fbe95d4a937a428961f7fcd0ecc99
Alexander Raab <alexander.raab1@gmx.de>
2022-01-05 09:19:56 +0100

    06p2

--- src/06.py
+++ src/06.py
@@ -2,16 +2,18 @@ def main(days):
     with open("..\\input\\06.txt",'r') as textfile:
         ages = [int(i) for i in textfile.readline().split(',')]
     
+    fishdict = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7 ,8], 0)
+    for i in ages:
+        fishdict[i] += 1
+    
     for _ in range(days):
-        i = 0
-        for count, value in enumerate(ages):
-            if not value:
-                ages[count] = 6
-                i += 1
-            else:
-                ages[count] -= 1
-        ages.extend([8]*i)
-    print("Amount of fish after",days,"days:", len(ages))
+        tmp = fishdict[0]
+        for i in range(8):
+            fishdict[i] = fishdict[i+1]
+        fishdict[6] += tmp
+        fishdict[8] = tmp
+    
+    print("Amount of fish after",days,"days:", sum(fishdict.values()))
 
 if __name__ == "__main__":
     main(80)
