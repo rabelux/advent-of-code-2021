3dc9f511815afa689645c3aa66f82804efe9b36a
Alexander Raab <alexander.raab1@gmx.de>
2022-01-14 11:17:54 +0100

    Update 07.py
    Refactor copy-paste-code

--- src/07.py
+++ src/07.py
@@ -1,28 +1,20 @@
-def part1(positions):
+def calc_fuel(positions, cumm_fuel = False):
     consumption = [0] * (max(positions) + 1)
-    for pos in range(len(consumption)):
-        fuel = 0
-        for i in positions:
-            fuel += abs(pos - i)
-        consumption[pos] = fuel
-    print("Min Consumption possible for part 1:", min(consumption))
-
-def part2(positions):
-    consumption = [0] * (max(positions) + 1)
+    f = lambda x: int(x * (x + 1) / 2) if cumm_fuel else x
     for pos in range(len(consumption)):
         fuel = 0
         for i in positions:
             distance = abs(pos - i)
-            fuel += distance * (distance + 1) / 2
+            fuel += f(distance)
         consumption[pos] = fuel
-    print("Min Consumption possible for part 2:", min(consumption))
+    return min(consumption)
 
 def main():
     with open("..\\input\\07.txt",'r') as textfile:
         positions = [int(i) for i in textfile.readline().split(',')]
-    
-    part1(positions)
-    part2(positions)
+    result_1 = calc_fuel(positions)
+    result_2 = calc_fuel(positions, True)
+    print("Result 1:", result_1, "Result 2:", result_2)
 
 if __name__ == "__main__":
     main()
