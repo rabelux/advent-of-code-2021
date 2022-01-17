3f52178dd56bf5015ad24ca1eb7119a3cbdee832
Alexander Raab <alexander.raab1@gmx.de>
2022-01-17 14:29:02 +0100

    08: More elegant and performant approach

--- src/08.py
+++ src/08.py
@@ -8,51 +8,36 @@ def part1(stringlist):
 
 def part2(left, right):
     int_res = []
+    segmcount2index = {2 : 1, 3 : 7, 4 : 4, 7: 8}
     for i in range(len(left)):
-        # assign values to variables
         digit_2_3_5 = []
         digit_0_6_9 = []
+        digits = [0] * 10
         for j in left[i]:
-            if len(j) == 2:
-                digit_1 = frozenset(j)
-            elif len(j) == 3:
-                digit_7 = frozenset(j)
-            elif len(j) == 4:
-                digit_4 = frozenset(j)
-            elif len(j) == 5: # 2, 3, 5
+            if len(j) == 5: # 2, 3, 5
                 digit_2_3_5.append(frozenset(j))
             elif len(j) == 6: # 0, 6, 9
                 digit_0_6_9.append(frozenset(j))
             else:
-                digit_8 = frozenset(j)
-        for digit in digit_2_3_5:
-            if digit_1 < digit:
-                digit_3 = digit
-            elif len(digit | digit_4) == 7:
-                digit_2 = digit
-            else:
-                digit_5 = digit
-        for digit in digit_0_6_9:
-            if not digit_1 < digit:
-                digit_6 = digit
-            elif digit_3 < digit:
-                digit_9 = digit
-            else:
-                digit_0 = digit
+                digits[segmcount2index[len(j)]] = frozenset(j)
         
-        lookup = {
-            digit_0:'0',
-            digit_1:'1',
-            digit_2:'2',
-            digit_3:'3',
-            digit_4:'4',
-            digit_5:'5',
-            digit_6:'6',
-            digit_7:'7',
-            digit_8:'8',
-            digit_9:'9'
-            }
-
+        for digit in digit_2_3_5:
+            if digits[1] < digit:
+                digits[3] = digit
+            elif len(digit | digits[4]) == 7:
+                digits[2] = digit
+            else:
+                digits[5] = digit
+        
+        for digit in digit_0_6_9:
+            if not digits[1] < digit:
+                digits[6] = digit
+            elif digits[3] < digit:
+                digits[9] = digit
+            else:
+                digits[0] = digit
+        
+        lookup = dict(zip(digits,'0123456789'))
         str_res = "".join([lookup.get(frozenset(s)) for s in right[i]])
         int_res.append(int(str_res))
     return sum(int_res)
